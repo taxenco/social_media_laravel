8b5c98bfd9995de4c4f59f558405b67c2752966d
taxenco <carlos.beltran.exposito@gmail.com>
2020-04-17 16:15:35 +0100

    login page design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,20 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+        <img src='https://image.flaticon.com/icons/svg/978/978012.svg' style="max-height:120px">
+        </div>
+        <div class="col-9">
+            <div><h1>Social Media</h1></div>
+            <div>
+                <div><strong>153</strong> Post</div>
+                <div><strong>23k</strong> Followers</div>
+                <div><strong>212</strong> Following</div>
             </div>
+            <div class="pt-4 font-weight-bold"><a href="https://github.com/taxenco/social_media_laravel">@CarlosBeltran</a></div>
+            <!-- <div class="pt-4 font-weight-bold">@CarlosBeltran</div> -->
+            <div>Social media project to learn coding on php and Laravel.</div>
         </div>
     </div>
 </div>
