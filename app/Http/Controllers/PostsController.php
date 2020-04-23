a4774c51be42f6fed5a7b007c3d49698597c5a90
taxenco <carlos.beltran.exposito@gmail.com>
2020-04-23 12:10:17 +0100

    picture profile

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Intervention\Image\ImageManagerStatic as Image;
+
 class PostsController extends Controller
 {
  public function __construct()
@@ -22,11 +24,22 @@ class PostsController extends Controller
 
   $imagePath = request('image')->store('upload', 'public');
 
+  $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
+
+  $image->save();
+
   auth()->user()->posts()->create([
    'caption' => $data['caption'],
    'image' => $imagePath,
   ]); //check user auth and post caption
 
-  return redirect('/profile/'.auth()->user()->id); //redirects to user profile after post
+  return redirect('/profile/' . auth()->user()->id); //redirects to user profile after post
  }
+
+ public function show(\App\Post $post)
+ {
+  return view('posts.show', compact('post'));
+
+ }
+
 }
